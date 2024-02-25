a212097ea0e35e8702ce1eede9f3509a9e20db6f
AlessandroCosta99 <alessandro8.costa@polimi.mail.it>
2024-02-25 01:36:20 +0000

    slight changes to mpc

--- trajectory_adaptation/python-scripts/optimizer_version_2.py
+++ trajectory_adaptation/python-scripts/optimizer_version_2.py
@@ -10,7 +10,6 @@ import matplotlib.pyplot as plt
 import scipy
 
 #ROS
-import moveit_commander
 import message_filters
 import rospy
 from std_msgs.msg import Float64MultiArray, MultiArrayDimension, MultiArrayLayout
@@ -36,8 +35,8 @@ class RobotController():
     def __init__(self):
         self.time_step = 0
         self.prev_time_step = 0
-        self.x_f = 0.6
-        self.y_f = -0.3
+        self.x_f = 400 #image frame
+        self.y_f = 180 #image frame
         self.num_int_points = 10
         self.trajectory_history = []
         self.stem_error_history = []
@@ -68,23 +67,23 @@ class RobotController():
         sync_cb = message_filters.ApproximateTimeSynchronizer(sync_sub,  10, 0.1, allow_headerless=True) 
         sync_cb.registerCallback(self.callback)
 
-    def franka_state_callback(self, msg):
-        self.robot_pose_init = Float64MultiArray()
-        self.robot_pose_init.data = [0.0] * 3
-        self.robot_pose_init.data[0] = msg.O_T_EE[12]
-        self.robot_pose_init.data[1] = msg.O_T_EE[13]
-        self.robot_pose_init.data[2] = msg.O_T_EE[14]
-        #update initial position with the data received
-        self.initial_position = np.array([self.robot_pose_init.data[0], self.robot_pose_init.data[1], self.robot_pose_init.data[2]])
-        self.initial_position_sub.unregister()
-        self.initial_position_sub = None
+    # def franka_state_callback(self, msg):
+    #     self.robot_pose_init = Float64MultiArray()
+    #     self.robot_pose_init.data = [0.0] * 3
+    #     self.robot_pose_init.data[0] = msg.O_T_EE[12]
+    #     self.robot_pose_init.data[1] = msg.O_T_EE[13]
+    #     self.robot_pose_init.data[2] = msg.O_T_EE[14]
+    #     #update initial position with the data received
+    #     self.initial_position = np.array([self.robot_pose_init.data[0], self.robot_pose_init.data[1], self.robot_pose_init.data[2]])
+    #     self.initial_position_sub.unregister()
+    #     self.initial_position_sub = None
 
     def callback(self, stem_pose, berry):
         center_hf = 0.00 #maybe 0.2
         self.dist_from_center = center_hf - stem_pose.data[0]
         self.stem_error_history.append(self.dist_from_center)
-        self.pred_berry_pose = np.array(berry.data).reshape((10, 2))
-        self.predicted_strawberry_position_history.append(self.pred_berry_pose)
+        self.pred_berry_pose = np.array(berry.data).reshape((1, 2))
+        #self.predicted_strawberry_position_history.append(self.pred_berry_pose)
 
     def gen_opt_traj(self):
         initial_theta = np.zeros(self.num_int_points + 1)
@@ -99,8 +98,8 @@ class RobotController():
         return cost
        
     def calculate_cost(self): #, points
-        return self.sum_squared_distances(self.pred_berry_pose) + self.dist_from_center**2
-
+        #return self.sum_squared_distances(self.pred_berry_pose) + self.dist_from_center**2
+        return np.linalg.norm(self.target_position-self.pred_berry_pose)**2 + self.dist_from_center**2
     def line_equation(self, x1, y1, x2, y2):
         m = (y2 - y1) / (x2 - x1)
         b = y1 - m * x1
@@ -138,7 +137,7 @@ class RobotController():
     
     def stack_constant_actions(self):
         optimal_actions = self.optimal_trajectory[1:11,:]
-        constant_values = np.array([[0.7], [0.726858], [0.0265717,], [0.686072]])
+        constant_values = np.array([[0.7], [0.726858], [0.0265717,], [0.686072]]) #check this
         new_columns = np.column_stack([constant_values[i] * np.ones((optimal_actions.shape[0],  1)) for i in range(4)])
         candidate_actions_full = np.hstack((optimal_actions, new_columns))
         self.pub_candidate_actions(candidate_actions_full)
@@ -182,5 +181,4 @@ class RobotController():
 if __name__ == '__main__':
     rospy.init_node('optimizer')
     mpc = RobotController()
-    mpc.loop()
     rospy.spin()
