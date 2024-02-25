c8ba8a69af7a0e46d7464652182703792a40b553
olazazat <oussama.lazazat@icloud.com>
2024-02-25 18:33:16 +0000

    one_time_prediction

---
+++ one_prediction_model/model_ann.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class StrawberryPredictor(nn.Module):
+    def __init__(self):
+        super(StrawberryPredictor, self).__init__()
+        self.fc1 = nn.Linear(8, 64)
+        self.fc2 = nn.Linear(64, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 2)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
